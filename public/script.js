d5417802409d0d9ee217044970ac5cfb8d4ffce0
movi1 <98534830+movi1@users.noreply.github.com>
2023-11-21 11:40:13 +0000

    add emoji

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+
 const num1Input = document.getElementById('num1');
 const num2Input = document.getElementById('num2');
 const calculateBtn = document.getElementById('calculate');
@@ -5,15 +6,23 @@ const resultDiv = document.getElementById('result');
 
 
 
-
+// Add an event listener to the calculate button
 calculateBtn.addEventListener('click', () => {
   const num1 = parseFloat(num1Input.value);
   const num2 = parseFloat(num2Input.value);
 
+    // Check if the entered values are valid numbers
   if (isNaN(num1) || isNaN(num2)) {
+    // Display an error message if the input is not valid
     resultDiv.innerHTML = 'Please enter valid numbers';
   } else {
+    // Calculate the sum of the entered numbers
     const sum = num1 + num2;
-    resultDiv.innerHTML = 'Result: ' + sum; 
+
+     // Display only the sum in the resultDiv
+     resultDiv.innerHTML = `tot = ${sum}`;
+
+    // Add a class to the result element
+    resultDiv.classList.add('show-emoji');
   }
 });
